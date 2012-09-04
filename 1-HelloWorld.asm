98bfc610b81e782820c8566555024b54390e878b
CodeBundle <codebundlers@gmail.com>
2012-09-04 10:00:45 +0200

    Update 1-HelloWorld.asm

--- 1-HelloWorld.asm
+++ 1-HelloWorld.asm
@@ -1,23 +1,23 @@
-;	Hello World.asm
+;	1-HelloWorld.asm
 ;	Written by "Sam Blumire"
 ;	Build Instructions:
 ;		Requirements:
 ;			Netwide Assembler (nasm) for linux. [sudo apt-get install nasm]
 ;		64 bit:
-;			nasm -f elf "Hello World.asm"
-;			ld -m elf_i386 -s -o hw "Hello World.o"
+;			nasm -f elf "1-HelloWorld.asm"
+;			ld -m elf_i386 -s -o helloworld "1-HelloWorld.o"
 ;		32 bit:
-;			nasm -f elf "Hello World.asm"
-;			ld -s -o hw "Hello World.o"
+;			nasm -f elf "1-HelloWorld.asm"
+;			ld -s -o hw "1-HelloWorld.o"
 ;	Run Instructions;
 ;		Terminal Application
 ;		No Root Access Needed
-;		Binary Compiled to "hw" (./hw from location of binary file)
+;		Binary Compiled to "helloworld" (./helloworld from location of binary file)
 
 
 SECTION .data					;Constants
-	msg: 	db "IT WORKS YAA", 10	;msg = Hello World(new Line)
-	msglen: 	equ $-msg		;msglen = Length of variable "msg"
+	msg: 	db "Hello World!", 10		;msg = Hello World(new Line)
+	len: 	equ $-msg			;msglen = Length of variable "msg"
 	
 
 SECTION .text					;Code
@@ -27,7 +27,7 @@ _start:						;Start of Script
 	mov eax, 4				;Kernel Function = sys_write
 	mov ebx, 1					;Standard output file descriptor
 	mov ecx, msg					;Message to Print
-	mov edx, msglen				;Number of Characters to print
+	mov edx, len				;Number of Characters to print
 	int 80h						;Call the Kernel
 	mov eax, 1				;Kernel Function = sys_exit
 	mov ebx, 0					;Error code (0 = no error)
